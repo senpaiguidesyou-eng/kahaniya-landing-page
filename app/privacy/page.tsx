5f535fb3470b41ceb2731d9127eb59fb8f68c270
Sameer Pasha <sampasha007@gmail.com>
2026-01-06 23:20:35 -0800

    Update Google Analytics

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -178,19 +178,7 @@ export default function PrivacyPage() {
             </section>
 
             <Footer />
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-3QT3EL5J28"
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
 
-          gtag('config', 'G-3QT3EL5J28');
-        `}
-            </Script>
         </main>
     )
 }
